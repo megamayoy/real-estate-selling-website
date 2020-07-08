5e496c8e14138a923f904406c4bad8daf4c2a9b3
megamayoy <mai.elshiashi@gmail.com>
2020-07-08 23:02:37 +0200

    create realtor model

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(blank=True)
+    is_seller_of_the_month = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
