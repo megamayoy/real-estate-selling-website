dd647bdbf1d8f9345a5236e2c9074efba7ec58f6
MaiElshiashi <mai.elshiashi@gmail.com>
2020-07-21 09:21:56 +0200

    add static folders locations in STATICFILES_DIRS list

--- real_estate_selling_website/settings.py
+++ real_estate_selling_website/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'real_estate_selling_website/static')
+]
 
 # Media settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
