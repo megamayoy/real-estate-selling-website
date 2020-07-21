827d24cc014db440458f3046c415fad7be6c7adb
MaiElshiashi <mai.elshiashi@gmail.com>
2020-07-21 10:42:13 +0200

    Admin Panel| realtors table | enable filtering by hire date

--- realtors/admin.py
+++ realtors/admin.py
@@ -7,6 +7,7 @@ class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'hire_date', 'email')
     list_display_links = ('id', 'name')
     list_per_page = 25
+    list_filter = ('hire_date',)
 
 
 admin.site.register(Realtor, RealtorAdmin)
