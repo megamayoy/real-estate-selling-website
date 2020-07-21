1acedda26f2c366768137b960dc6a8fa5557630c
MaiElshiashi <mai.elshiashi@gmail.com>
2020-07-21 10:13:18 +0200

    Admin Panel | listings table | make id and title clickable

--- listings/admin.py
+++ listings/admin.py
@@ -7,6 +7,7 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'title', 'is_published', 'list_date', 'price', 'realtor'
     )
+    list_display_links = ('id', 'title')
 
 
 admin.site.register(Listing, ListingAdmin)
