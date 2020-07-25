533e0eb0247feadece1739e7177fee251626747d
MaiElshiashi <mai.elshiashi@gmail.com>
2020-07-25 16:17:14 +0200

    create initial static single listing page (squashed)

--- listings/urls.py
+++ listings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from listings import views
 
 urlpatterns = [
-    path('', views.listings, name="listings")
+    path('', views.listings, name="listings"),
+    path('<int:listing_id>', views.listing, name="listing")
 ]
--- listings/views.py
+++ listings/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def listings(request):
     return render(request, 'listings/listings.html')
+
+
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html')
