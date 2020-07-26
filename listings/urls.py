2809c8e03744177bdc4c99ba9366faf9965304f7
MaiElshiashi <mai.elshiashi@gmail.com>
2020-07-26 20:41:22 +0200

    add forward slash to url path

--- listings/urls.py
+++ listings/urls.py
@@ -3,5 +3,5 @@ from listings import views
 
 urlpatterns = [
     path('', views.listings, name="listings"),
-    path('<int:listing_id>', views.listing, name="listing")
+    path('<int:listing_id>/', views.listing, name="listing")
 ]
