43a234dd0990d523d215edf58784f240d5f2f300
MaiElshiashi <mai.elshiashi@gmail.com>
2020-07-31 22:45:46 +0200

    enable pagination in listings page

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator
 
 
 def listings(request):
     listings_list = Listing.objects.all().select_related('realtor')
+    paginator = Paginator(listings_list, per_page=3)
+    requested_page = request.GET.get('page')
+    listings_page = paginator.get_page(requested_page)
     return render(
-        request, 'listings/listings.html', context={'listings': listings_list}
+        request, 'listings/listings.html', context={'listings': listings_page}
     )
 
 
