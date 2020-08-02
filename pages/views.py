72a8cdc6c13bdbf411d0b18a805861091b732dd0
MaiElshiashi <mai.elshiashi@gmail.com>
2020-08-02 02:15:47 +0200

    show the latest 3 listings on home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from listings.models import Listing
 
 def index(request):
-    return render(request, 'pages/index.html')
+    latest_listings = Listing.objects.order_by('-list_date').filter(
+        is_published=True
+    ).select_related('realtor')[:3]
+    return render(
+        request, 'pages/index.html', {'latest_listings': latest_listings}
+    )
 
 
 def about(request):
